3fd5b5520e22903abcf46fde775e2b6ab3542c4d
Emirhan Inan <159465946+emirhaninan@users.noreply.github.com>
2025-05-03 13:31:10 +0300

    Update port

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def run():
             filtered_data = data_filtered
         return filtered_data.to_dict('records')
 
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8500))
     app.run_server(debug=False, host='0.0.0.0', port=port)
 
 # Allows run as script OR via main.py import
