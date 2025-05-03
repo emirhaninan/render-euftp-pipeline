aaf58c8fff09419d16ff640b3e83c66519793bde
Emirhan Inan <neratygrassfore@gmail.com>
2025-05-03 09:39:10 +0300

    Render memory threshold--manually add excel files

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import step1_generate_calls
+"""import step1_generate_calls
 import step2_process_calls
 import step3_generate_researchers
 import step4_process_researchers
-import step5_match_data
+import step5_match_data"""
 import step6_create_dashboard  # Dashboard's run() will launch the web server
 
-def prepare_data():
+"""def prepare_data():
     step1_generate_calls.run()
     step2_process_calls.run()
     step3_generate_researchers.run()
     step4_process_researchers.run()
-    step5_match_data.run()
+    step5_match_data.run()"""
 
 if __name__ == '__main__':
-    prepare_data()
+    """prepare_data()"""
     step6_create_dashboard.run()
